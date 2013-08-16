8e0d18ffdf1edf46319374af01a04f4fdea61916
Suhail Gupta <suhailgupta03@gmail.com>
2013-08-16 12:33:06 +0530

    cloud-analyst (first commit)
    A netbeans project

---
+++ cloud analyst/src/cloud/analyst/CloudAnalyst.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cloud.analyst;
+
+/**
+ *
+ * @author user
+ */
+public class CloudAnalyst {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
